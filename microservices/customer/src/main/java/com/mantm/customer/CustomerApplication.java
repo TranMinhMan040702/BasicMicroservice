d192a936267279ad321f31646cfe13405a6a1919
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2023-06-08 15:16:58 +0700

    config rabbit mq

--- microservices/customer/src/main/java/com/mantm/customer/CustomerApplication.java
+++ microservices/customer/src/main/java/com/mantm/customer/CustomerApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = "com.mantm.amqp")
 @EnableEurekaClient
-@EnableFeignClients(
-        basePackages = "com.mantm.clients"
-)
+@EnableFeignClients(basePackages = "com.mantm.clients")
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- microservices/notification/src/main/java/com/mantm/notification/NotificationApplication.java
+++ microservices/notification/src/main/java/com/mantm/notification/NotificationApplication.java
@@ -1,13 +1,10 @@
 package com.mantm.notification;
 
-import com.mantm.amqp.RabbitMQMessageProducer;
-import com.mantm.notification.config.NotificationConfig;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication (scanBasePackages = {
         "com.mantm.amqp",
@@ -18,17 +15,5 @@ import org.springframework.context.annotation.Bean;
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(
-            RabbitMQMessageProducer producer,
-            NotificationConfig notificationConfig) {
-
-        return args -> {
-            producer.publish("iphone14",
-                    notificationConfig.getInternalExchange(),
-                    notificationConfig.getInternalNotificationRoutingKey());
-        };
     }
 }
