ab397c0ac35234038ae84feab102f090e1169ac0
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2023-06-08 08:20:39 +0700

    create producer

--- microservices/notification/src/main/java/com/mantm/notification/NotificationApplication.java
+++ microservices/notification/src/main/java/com/mantm/notification/NotificationApplication.java
@@ -1,15 +1,34 @@
 package com.mantm.notification;
 
+import com.mantm.amqp.RabbitMQMessageProducer;
+import com.mantm.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = {
+        "com.mantm.amqp",
+        "com.mantm.notification"
+})
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.mantm.clients")
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig) {
+
+        return args -> {
+            producer.publish("iphone14",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
     }
 }
